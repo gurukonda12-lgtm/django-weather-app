f28898cfb36fcb1aa4d906fb7bedd928493739a8
gurukonda12-lgtm <gurukonda12@gmail.com>
2026-03-12 11:50:47 +0530

    Configure static files for deployment

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Optional: extra directories for development
+STATICFILES_DIRS = [
+    BASE_DIR / "weatherapp" / "static",
+]
